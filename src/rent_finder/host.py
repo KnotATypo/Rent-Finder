b508e67a68ed1b20ece1ca9601707b1cf1b1a440
Josh <joshua.mackay17@gmail.com>
2026-02-02 22:40:18 +1000

    Basic hosting

---
+++ src/rent_finder/host.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from rent_finder.logger import logger, configure_logging
+from waitress import serve
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+def start():
+    logger.info("Starting Flask app...")
+    serve(app, host="0.0.0.0", port=4321)
+
+
+if __name__ == "__main__":
+    configure_logging()
+    start()
