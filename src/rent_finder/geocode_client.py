ea351f956f1a904c102c305b6e9e14bdea11cb7e
Josh <joshua.mackay17@gmail.com>
2026-05-07 05:30:24 +1000

    Geocode client now respects rate limits

--- src/rent_finder/geocode_client.py
+++ src/rent_finder/geocode_client.py
@@ -1,5 +1,7 @@
 import os
 import re
+from datetime import datetime, timedelta
+from time import sleep
 
 import requests
 
@@ -15,7 +17,18 @@ class GeocodeClient:
     Client to forward geocode addresses to coordinates using https://geocode.maps.co/.
     """
 
-    api_key = os.getenv("GEOCODE_API_KEY")
+    api_key: str
+    rate_limit_start: datetime
+    last_request: datetime
+
+    def __init__(self) -> None:
+        key = os.getenv("GEOCODE_API_KEY")
+        if key is None:
+            raise RuntimeError("GEOCODE_API_KEY not set")
+
+        self.api_key = key
+        self.rate_limit_start = datetime.min
+        self.last_request = datetime.min
 
     def get_coordinate(self, address: str) -> tuple[None, None] | tuple[float, float]:
         """
@@ -29,10 +42,27 @@ class GeocodeClient:
         elif re.match(r"^\d+ \d", address):
             address = address[address.index(" ") + 1 :]
 
-        response = requests.get(f"https://geocode.maps.co/search?q={address}&api_key={self.api_key}")
+        if self.rate_limit_start > datetime.now() - timedelta(hours=12):
+            # If it has been less than 12 hours since we hit a rate limit, make sure we respect limits
+            sleep(1 - ((self.last_request - datetime.now()).microseconds / 1e-6))
 
-        if response.status_code != 200:
-            raise StatusException(f"{response.status_code}: {response.text}")
+        response = None
+        while response is None:
+            self.last_request = datetime.now()
+            response = requests.get(f"https://geocode.maps.co/search?q={address}&api_key={self.api_key}")
+
+            if response.status_code == 429:
+                logger.warning("Geocode: Rate limit reached")
+                self.rate_limit_start = datetime.now()
+                sleep(1)
+                response = None
+            elif response.status_code == 503:
+                logger.warning("Geocode: Service unavailable, retrying")
+                # High server load, pause then try again
+                sleep(5)
+                response = None
+            elif response.status_code != 200:
+                raise StatusException(f"{response.status_code}: {response.text}")
 
         response = response.json()
         if len(response) == 0:
